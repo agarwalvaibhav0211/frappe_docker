de80134dbb3a3a1c631046a0670c0edd10a9139c
Vaibhav Agarwal <agarwalvaibhav0211@gmail.com>
2024-04-12 17:37:08 +0530

    Indian ERP

---
+++ build.sh
@@ -0,0 +1,9 @@
+export APPS_JSON_BASE64=$(base64 -w 0 $PWD/images/indian_erp/apps.json)
+docker build \
+  --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
+  --build-arg=FRAPPE_BRANCH=version-15 \
+  --build-arg=PYTHON_VERSION=3.11.6 \
+  --build-arg=NODE_VERSION=18.18.2 \
+  --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --tag=ghcr.io/user/repo/custom:1.0.0 \
+  --file=images/indian_erp/Containerfile .
