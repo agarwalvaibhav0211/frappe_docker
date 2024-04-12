0ed025577edbb8eeb433a1e929f252bab074130e
Vaibhav Agarwal <agarwalvaibhav0211@gmail.com>
2024-04-12 19:01:45 +0000

    multi arch build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 export APPS_JSON_BASE64=$(base64 -w 0 $PWD/images/indian_erp/apps.json)
 # echo $APPS_JSON_BASE64
 docker buildx build \
-  --platform linux/arm/v7,linux/arm64/v8,linux/amd64,linux/arm64,linux/arm/v6 \
+  --platform linux/arm64/v8,linux/amd64,linux/arm64 \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-15 \
   --build-arg=PYTHON_VERSION=3.11.6 \
   --build-arg=NODE_VERSION=18.18.2 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  --tag=indian-erpnext:v15.20.3 \
-  --file=images/indian_erp/Containerfile .
+  --tag=freeroamer90/erpnext-multi-arch-indian-compliance:v15.20.3 \
+  --file=images/indian_erp/Containerfile . --push
